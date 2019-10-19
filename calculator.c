e3ee9ef8233617d42aade3ead6699df30e6cba5a
prat11082002mkl <sprathyush01@gmail.com>
2019-10-19 06:32:50 -0700

    this is my new calculator

---
+++ calculator.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+	int sum(int a,int b)
+	return a+b;
+	
+}
+int main(){
+int a=5,b=10;
+printf("sum = %d\n",sum(a,b));
+printf("diff = %d\n",diff(a,b));
+return 0;
+}
